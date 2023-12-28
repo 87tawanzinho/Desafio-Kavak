1fd233c689fd55b45d5df07b5e2bc0597363a83d
tawan <87tawanzinho@gmail.com>
2023-12-28 16:04:11 -0300

    resolving px

--- app/components/MainSection.tsx
+++ app/components/MainSection.tsx
@@ -8,14 +8,16 @@ export default function Mainsection() {
           src="https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjkxNjczL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNzAzMjY1OTc2MjQ4LmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
           className="w-screen lg:w-72  object-contain"
         ></img>
-        <div className=" pt-2 flex flex-col gap-2 px-4">
-          <p className="font-bold">Honda Fit</p>
-          <p className="text-sm border-b-2 border-gray-100">2017 72.591km</p>
-          <p className="mt-2 text-gray-400">Preço à vista</p>
-          <p className="font-bold text-xl text-gray-800">
+        <div className=" pt-2 flex flex-col gap-2 ">
+          <p className="font-bold px-4">Honda Fit</p>
+          <p className="text-sm border-b-2 border-gray-100 px-4">
+            2017 72.591km
+          </p>
+          <p className="mt-2 text-gray-400 px-4">Preço à vista</p>
+          <p className="font-bold text-xl text-gray-800 px-4">
             <span className="text-sm text-gray-600 mr-1">R$</span>69.2999
           </p>
-          <p className=" mt-4 border-t-2 pt-4 pb-4 border-gray-100 text-sm flex items-center gap-2 ">
+          <p className="p-4 mt-4 border-t-2 pt-4 pb-4 border-gray-100 text-sm flex items-center gap-2 text-gray-700 ">
             <Image src={gps} alt="gps" className="h-4 w-4 object-cover" />
             São paulo
           </p>
