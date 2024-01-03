35c7c2c7dca648fbce13ecac3e22a9c200c7e3cd
tawan <87tawanzinho@gmail.com>
2024-01-03 01:05:33 -0300

    finished mobile

--- app/[carId]/page.tsx
+++ app/[carId]/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 import React from 'react'
 import { CarI } from '../interface/CarInterface'
+import Link from 'next/link'
 
 function page({ params, searchParams }: { params: { carId: string }; searchParams: CarI }) {
   const carInformations = TakeThisCar(searchParams)
   return (
-    <div className='flex flex-col lg:flex-row  justify-center  '>
+    <div className='flex flex-col lg:flex-row  justify-center pb-4 '>
       <div className='h-2/4   '>
         <p className='text-green-800 p-4 w-full'>
           {carInformations.brand} {'>'} {carInformations.name}
@@ -14,17 +15,25 @@ function page({ params, searchParams }: { params: { carId: string }; searchParam
           src={`${carInformations.photo}`}
           className='w-full lg:w-4/5 h-4/5  object-cover rounded-none lg:rounded-lg'
         />
-        <div>
-          <p className='text-xl mt-10 font-bold'>Informações básicas</p>
+        <div className='p-2 '>
+          <p className='text-xl   font-bold'>Informações básicas</p>
           <img
             src='https://thumbs.dreamstime.com/b/het-bedrijfsconcept-bedrijfsvrouw-die-de-banner-houden-huurt-me-73893545.jpg'
             alt='hire me'
             className='h-40 w-40'
           />
+          <p className='text-2xl mt-8 font-bold '>Motivos para me contratar:</p>
+          <p className='w-11/12 lg:w-96'>
+            Posso não ser o melhor do mundo no momento, meu codigo pode não ser perfeito, posso não ser o melhor
+            indicado ou o melhor currículo, mas posso fazer acontecer..{' '}
+            <span className='text-blue-400 font-bold'>Quem acreditou</span> em você quando você ainda não era um
+            recrutador?
+          </p>
+          <p className='text-pink-600 font-bold'>Só preciso da sua oportunidade {'(;'}</p>
         </div>
       </div>
 
-      <div className='border border-gray-200  rounded w-1/4  h-2/4 mt-14 p-4 '>
+      <div className='border border-gray-200  rounded w-full lg:w-1/4  h-2/4 mt-14 p-4 '>
         <h2 className='font-bold text-2xl'>
           {carInformations.brand} {carInformations.name}
         </h2>
@@ -46,18 +55,20 @@ function page({ params, searchParams }: { params: { carId: string }; searchParam
           <p className='font-bold'>{carInformations.model}</p>
         </div>
 
-        <div className='p-4'>
-          <button className='cursor-pointer relative group overflow-hidden border-2 w-full px-0 pt-2  border-green-500'>
-            <span className='font-bold text-white text-xl relative z-10 group-hover:text-green-500 duration-500'>
-              COMPRAR AGORA
-            </span>
-            <span className='absolute top-0 left-0 w-full bg-green-500 duration-500 group-hover:-translate-x-full h-full'></span>
-            <span className='absolute top-0 left-0 w-full bg-green-500 duration-500 group-hover:translate-x-full h-full'></span>
+        <Link href={'https://wa.link/narclt'} target='blank'>
+          <div className='mt-10'>
+            <button className='cursor-pointer relative group overflow-hidden border-2 w-full px-0 pt-2  border-green-500'>
+              <span className='font-bold text-white text-xl relative z-10 group-hover:text-green-500 duration-500'>
+                COMPRAR AGORA
+              </span>
+              <span className='absolute top-0 left-0 w-full bg-green-500 duration-500 group-hover:-translate-x-full h-full'></span>
+              <span className='absolute top-0 left-0 w-full bg-green-500 duration-500 group-hover:translate-x-full h-full'></span>
 
-            <span className='absolute top-0 left-0 w-full bg-green-500 duration-500 delay-300 group-hover:-translate-y-full h-full'></span>
-            <span className='absolute delay-300 top-0 left-0 w-full bg-green-500 duration-500 group-hover:translate-y-full h-full'></span>
-          </button>
-        </div>
+              <span className='absolute top-0 left-0 w-full bg-green-500 duration-500 delay-300 group-hover:-translate-y-full h-full'></span>
+              <span className='absolute delay-300 top-0 left-0 w-full bg-green-500 duration-500 group-hover:translate-y-full h-full'></span>
+            </button>
+          </div>
+        </Link>
       </div>
     </div>
   )
